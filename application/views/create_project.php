0ec327e14805951960045c015aed2e31c2eeaae7
Nuzha <fadilanuzha@gmail.com>
2014-06-29 21:14:57 +0530

    recent activity modified

--- application/views/create_project.php
+++ application/views/create_project.php
@@ -149,31 +149,39 @@ $(function(){
                   </a>
                   <a href="#" class="list-group-item">
                     <span class="badge">4 minutes ago</span>
-                    <i class="fa fa-comment"></i> Commented on a post
+                    <i class="fa fa-comment"></i> You commented on a post
                   </a>
                   <a href="#" class="list-group-item">
                     <span class="badge">23 minutes ago</span>
-                    <i class="fa fa-truck"></i> Order 392 shipped
+                    <i class="fa fa-truck"></i> User Story #10 of Project 01 modified
                   </a>
                   <a href="#" class="list-group-item">
                     <span class="badge">46 minutes ago</span>
-                    <i class="fa fa-money"></i> Invoice 653 has been paid
+                    <i class="fa fa-money"></i> New user stories has been added for Project 02
                   </a>
                   <a href="#" class="list-group-item">
                     <span class="badge">1 hour ago</span>
-                    <i class="fa fa-user"></i> A new user has been added
+                    <i class="fa fa-user"></i> A new user has been added for Project 02
                   </a>
                   <a href="#" class="list-group-item">
                     <span class="badge">2 hours ago</span>
-                    <i class="fa fa-check"></i> Completed task: "pick up dry cleaning"
+                    <i class="fa fa-check"></i> Completed task: "create Administrator login"
+                  </a>
+                  <a href="#" class="list-group-item">
+                    <span class="badge">1 hour ago</span>
+                    <i class="fa fa-globe"></i> Completed user story: "As scrum master I need to add copy or delete work items when...."
                   </a>
                   <a href="#" class="list-group-item">
                     <span class="badge">yesterday</span>
-                    <i class="fa fa-globe"></i> Saved the world
-                  </a>
-                  <a href="#" class="list-group-item">
-                    <span class="badge">two days ago</span>
                     <i class="fa fa-check"></i> Completed task: "fix error on sales page"
+                  </a>
+                   <a href="#" class="list-group-item">
+                    <span class="badge">two days ago</span>
+                    <i class="fa fa-check"></i> Completed task: "fix error on login page"
+                  </a>
+                   <a href="#" class="list-group-item">
+                    <span class="badge">two days ago</span>
+                    <i class="fa fa-check"></i> New user story assigned: "As scrum master I want to view reports so that I can track ..."
                   </a>
                 </div>
                 <div class="text-right">
