03545693b16b2cf95ab467d481b037250e1166a3
Yehan Pemarathne <yehancha@gmail.com>
2014-08-17 23:53:48 +0530

    Notification badge aligned to left

--- application/views/notification.php
+++ application/views/notification.php
@@ -4,9 +4,9 @@
         $query_resource = mysql_query($sql);
         while ($project = mysql_fetch_assoc($query_resource)):
     ?>
-            <a href="#" class="list-group-item">
-                <span class="badge"><?php echo $project['changetype']; ?></span>
-                <i class="fa fa-calendar"></i> <?php echo $project['msg']; ?>
+            <a href="#" class="list-group-item row">
+                <span class="badge col-sm-3" style="float: left;"><?php echo $project['changetype']; ?></span>
+                <div class="col-sm-9"><i class="fa fa-calendar"></i> <?php echo $project['msg']; ?></div>
             </a>
     <?php
         endwhile;
