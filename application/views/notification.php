3b9c7c38a1289493b822622f66282a7c3cb8d023
Nuza <fadilanuzha@gmail.com>
2014-08-17 23:38:29 -0700

    Notifications preveiw

--- application/views/notification.php
+++ application/views/notification.php
@@ -1,12 +1,12 @@
 <html>
     <?php
-        $sql = "SELECT `name`, `changetype` FROM `user_stories`, `record_notifications` WHERE `record_notifications`.`usid`=`user_stories`.`StoryId`";
+        $sql = "SELECT `msg`, `changetype` from `record_notifications`";
         $query_resource = mysql_query($sql);
         while ($project = mysql_fetch_assoc($query_resource)):
     ?>
             <a href="#" class="list-group-item">
                 <span class="badge"><?php echo $project['changetype']; ?></span>
-                <i class="fa fa-calendar"></i> <?php echo $project['name']; ?>
+                <i class="fa fa-calendar"></i> <?php echo $project['msg']; ?>
             </a>
     <?php
         endwhile;
